f5d1e2f5364dbc360eea2b66b30c31b300da8f39
valmon55 <fedor_ka@mail.ru>
2024-02-24 17:07:18 +0300

    берем XMR.HomeApp из прошлого модуля - Module 37

---
+++ XMR/XMR.HomeApp/XMR.HomeApp/Pages/DevicePage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XMR.HomeApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DevicePage : ContentPage
+    {
+        public DevicePage()
+        {
+            InitializeComponent();
+            GetDevices();
+        }
+
+        /// <summary>
+        /// Метод для выгрузки устройств
+        /// </summary>
+        public void GetDevices()
+        {
+            // Создадим некий список устройств.
+            // В реальном приложении они могут доставаться из базы или веб-сервиса.
+            var homeDevices = new List<string>()
+           {
+               "Чайник",
+               "Стиральная машина",
+               "Посудомоечная машина",
+               "Мультиварка",
+               "Водонагреватель",
+               "Плита",
+               "Микроволновая печь",
+               "Духовой шкаф",
+               "Холодильник",
+               "Увлажнитель воздуха",
+               "Телевизор",
+               "Пылесос",
+               "Музыкальный центр",
+               "Компьютер",
+               "Игровая консоль"
+           };
+
+            // Создадим новый стек
+            var innerStack = new StackLayout();
+
+            // Сохраним в стек имеющиеся данные, используя свойство Children
+            foreach (string deviceName in homeDevices)
+            {
+                var deviceLabel = new Label() { Text = $"   {deviceName}", FontSize = 17 };
+                innerStack.Children.Add(new Label());
+                innerStack.Children.Add(deviceLabel);
+            }
+
+            // Сохраним стек внутрь уже имеющегося в xaml-файле блока прокручиваемой разметки
+            scrollView.Content = innerStack;
+        }
+    }
+}
---
+++ XMR/XMR.HomeApp/XMR.HomeApp/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XMR.HomeApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+        private void register_click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
