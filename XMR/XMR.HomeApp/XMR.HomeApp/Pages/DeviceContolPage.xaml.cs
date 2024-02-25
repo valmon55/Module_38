d9c5fd5691ed35acb1e1656a0fe26f1514b1550d
valmon55 <fedor_ka@mail.ru>
2024-02-25 16:20:28 +0300

    +

--- XMR/XMR.HomeApp/XMR.HomeApp/Pages/DeviceContolPage.xaml.cs
+++ XMR/XMR.HomeApp/XMR.HomeApp/Pages/DeviceContolPage.xaml.cs
@@ -47,12 +47,59 @@ namespace XMR.HomeApp.Pages
             stackLayout.Children.Add(timePickerText);
             stackLayout.Children.Add(timePicker);
 
+            // Создаем меню выбора в виде выпадающего списка с текстовым заголовком
+            var pickerText = new Label { Text = "Напряжение сети, В", Margin = new Thickness(0, 20, 0, 0) };
+            var picker = new Picker { Title = "Выберите напряжение сети" };
+            // Добавляем значения выпадающего списка для пользовательского выбора
+            picker.Items.Add("220");
+            picker.Items.Add("120");
+            // Добавляем элементы на страницу
+            stackLayout.Children.Add(pickerText);
+            stackLayout.Children.Add(picker);
+
+            //// Установим текст текущего значения переключателя Stepper
+            //var stepperText = new Label
+            //{
+            //    Text = "Температура: 5.0 °C",
+            //    HorizontalOptions = LayoutOptions.Center,
+            //    Margin = new Thickness(0, 30, 0, 0)
+            //};
+            //// Установим сам переключатель
+            //Stepper stepper = new Stepper
+            //{
+            //    Minimum = -30,
+            //    Maximum = 30,
+            //    Increment = 1,
+            //    Value = 5,
+            //    HorizontalOptions = LayoutOptions.Center,
+            //    VerticalOptions = LayoutOptions.CenterAndExpand
+            //};
+            //// Добавим в разметку
+            //stackLayout.Children.Add(stepperText);
+            //stackLayout.Children.Add(stepper);
+
+            Slider slider = new Slider
+            {
+                Minimum = -30,
+                Maximum = 30,
+                Value = 5.0,
+                ThumbColor = Color.DodgerBlue,
+                MinimumTrackColor = Color.DodgerBlue,
+                MaximumTrackColor = Color.Gray
+            };
+            var sliderText = new Label { Text = $"Температура: {slider.Value} °C", HorizontalOptions = LayoutOptions.Center, Margin = new Thickness(0, 30, 0, 0) };
+            stackLayout.Children.Add(sliderText);
+            stackLayout.Children.Add(slider);
+
             stackLayout.Children.Add(new Button { Text = "Сохранить", BackgroundColor = Color.Silver, Margin = new Thickness(0, 5, 0, 0) });
 
             // Регистрируем обработчик события выбора даты
             datePicker.DateSelected += (sender, e) => DateSelectedHandler(sender, e, datePickerText);
             // Регистрируем обработчик события выбора времени
             timePicker.PropertyChanged += (sender, e) => TimeChangedHandler(sender, e, timePickerText, timePicker);
+            // Регистрируем обработчик события выбора температуры
+            //stepper.ValueChanged += (sender, e) => TempChangedHandler(sender, e, stepperText);
+            slider.ValueChanged += (sender, e) => TempChangedHandler(sender, e, sliderText);
         }
 
         public void DateSelectedHandler(object sender, DateChangedEventArgs e, Label datePickerText)
@@ -65,6 +112,13 @@ namespace XMR.HomeApp.Pages
             // Обновляем текст сообщения, когда появляется новое значение времени
             if (e.PropertyName == "Time")
                 timePickerText.Text = "В " + timePicker.Time;
+        }
+        /// <summary>
+        /// Обработчик изменения температуры
+        /// </summary>
+        private void TempChangedHandler(object sender, ValueChangedEventArgs e, Label header)
+        {
+            header.Text = String.Format("Температура: {0:F1}°C", e.NewValue);
         }
     }
 }
