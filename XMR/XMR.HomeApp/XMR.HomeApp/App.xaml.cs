20db203da40944a288131da29bbf78ed6e7c3648
valmon55 <fedor_ka@mail.ru>
2024-02-25 13:07:33 +0300

    картинка фоном

--- XMR/XMR.HomeApp/XMR.HomeApp/App.xaml.cs
+++ XMR/XMR.HomeApp/XMR.HomeApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XMR.HomeApp
         {
             InitializeComponent();
 
-            MainPage = new DevicePage();
+            MainPage = new RegisterPage();
         }
 
         protected override void OnStart()
