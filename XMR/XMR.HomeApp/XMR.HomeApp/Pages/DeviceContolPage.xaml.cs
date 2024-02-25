6609799943e1c4151a94cd8f37642a0c784ea651
valmon55 <fedor_ka@mail.ru>
2024-02-25 15:59:59 +0300

    виджеты - дата и время

---
+++ XMR/XMR.HomeApp/XMR.HomeApp/Pages/DeviceContolPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XMR.HomeApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DeviceContolPage : ContentPage
+    {
+        public DeviceContolPage()
+        {
+            InitializeComponent();
+            GetContent();
+        }
+        public void GetContent()
+        {
+            // Создаем виджет выбора даты
+            var datePicker = new DatePicker
+            {
+                Format = "D",
+                // Диапазон дат: +/- неделя
+                MaximumDate = DateTime.Now.AddDays(7),
+                MinimumDate = DateTime.Now.AddDays(-7),
+            };
+
+            var datePickerText = new Label { Text = "Дата запуска ", Margin = new Thickness(0, 20, 0, 0) };
+
+            // Добавляем всё на страницу
+            stackLayout.Children.Add(new Label { Text = "Устройство" });
+            stackLayout.Children.Add(new Entry { BackgroundColor = Color.AliceBlue, Text = "Холодильник" });
+            stackLayout.Children.Add(datePickerText);
+            stackLayout.Children.Add(datePicker);
+
+            // Виджет выбора времени.
+            var timePickerText = new Label { Text = "Время запуска ", Margin = new Thickness(0, 20, 0, 0) };
+            var timePicker = new TimePicker
+            {
+                Time = new TimeSpan(13, 0, 0)
+            };
+
+            stackLayout.Children.Add(timePickerText);
+            stackLayout.Children.Add(timePicker);
+
+            stackLayout.Children.Add(new Button { Text = "Сохранить", BackgroundColor = Color.Silver, Margin = new Thickness(0, 5, 0, 0) });
+
+            // Регистрируем обработчик события выбора даты
+            datePicker.DateSelected += (sender, e) => DateSelectedHandler(sender, e, datePickerText);
+            // Регистрируем обработчик события выбора времени
+            timePicker.PropertyChanged += (sender, e) => TimeChangedHandler(sender, e, timePickerText, timePicker);
+        }
+
+        public void DateSelectedHandler(object sender, DateChangedEventArgs e, Label datePickerText)
+        {
+            // При срабатывании выбора - будет меняться информационное сообщение.
+            datePickerText.Text = "Запустится " + e.NewDate.ToString("dd/MM/yyyy");
+        }
+        public void TimeChangedHandler(object sender, PropertyChangedEventArgs e, Label timePickerText, TimePicker timePicker)
+        {
+            // Обновляем текст сообщения, когда появляется новое значение времени
+            if (e.PropertyName == "Time")
+                timePickerText.Text = "В " + timePicker.Time;
+        }
+    }
+}
